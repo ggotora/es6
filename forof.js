953aee367d20464d2dec2cec415bc78634891d36
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-04-21 05:49:13 +0200

    initial

---
+++ forof.js
@@ -0,0 +1,7 @@
+const days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+for (let day of days){
+    
+   console.log(day.charAt(0).toUpperCase() + day.slice(1));
+    
+}
